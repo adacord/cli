e20528a627e30a21d12d1397a220ccce782546ec
Alexandre M. Savio <alexsavio@gmail.com>
2021-10-22 17:16:35 +0200

    fix(cli): move the data push command to bucket load

--- src/adacord/cli/main.py
+++ src/adacord/cli/main.py
@@ -1,8 +1,7 @@
 import typer
 
-from . import data, user, bucket
+from . import user, bucket
 
 app = typer.Typer()
 app.add_typer(user.app, name="user")
 app.add_typer(bucket.app, name="bucket")
-app.add_typer(data.app, name="data")
